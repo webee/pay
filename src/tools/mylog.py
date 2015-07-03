eb8442fc2fb7d835b935b12fa1075cd6822273c2
webee <webee.yw@gmail.com>
2015-07-03 13:53:20 +0800

    add.

---
+++ src/tools/mylog.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+from __future__ import unicode_literals, print_function
+
+__author__ = 'webee'
+
+import logging
+
+
+def get_logger(name='main', level=logging.DEBUG):
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    if len(logger.handlers) > 0:
+        return logger
+
+    ch = logging.StreamHandler()
+    ch.setLevel(level)
+    formatter = logging.Formatter(
+        '%(asctime)s - [%(name)s.%(levelname)s] [%(threadName)s, %(module)s.%(funcName)s@%(lineno)d] %(message)s')
+    ch.setFormatter(formatter)
+
+    logger.addHandler(ch)
+
+    return logger

975ee7dae2952fb5a617bf4c02931e180cb22a8d
webee <webee.yw@gmail.com>
2015-07-06 17:45:55 +0800

    add pay api.

--- db/schema.sql
+++ db/schema.sql
@@ -10,17 +10,22 @@ create table trade_types(
 ) ;
 insert into trade_types values(1, '支付');
 insert into trade_types values(2, '提现');
-insert into trade_types values(3, '转账');
-insert into trade_types values(4, '退款');
+insert into trade_types values(3, '退款');
 
-CREATE TABLE pay_trades(
+CREATE TABLE pay_trade_records(
   id int AUTO_INCREMENT PRIMARY KEY ,
   trade_type int NOT NULL ,
-  source_channel varchar(128) NOT NULL ,
+  trade_serial_no VARCHAR(50) NOT NULL ,
+  source_channel VARCHAR(128) NOT NULL ,
   source_order_id VARCHAR(60) NOT NULL ,
-  source_order_name VARCHAR(254) NOT NULL ,
-  payer VARCHAR(254) ,
-  amount DECIMAL(12,2),
-  created_at DATETIME DEFAULT current_timestamp,
+  source_user_id VARCHAR(32) NOT NULL ,
+  source_account VARCHAR(32) NOT NULL ,
+  amount DECIMAL(12,2) ,
+  to_account VARCHAR(32) NOT NULL ,
+  product_name VARCHAR(50) NOT NULL ,
+  product_cat VARCHAR(50) NOT NULL ,
+  product_desc VARCHAR(50) NOT NULL ,
+  status VARCHAR(32) NOT NULL ,
+  created_at TIMESTAMP DEFAULT current_timestamp,
   FOREIGN KEY pay_trades_type (trade_type) REFERENCES trade_types (id)
 );
--- src/pay_site/mods/trade/views.py
+++ src/pay_site/mods/trade/views.py
@@ -2,47 +2,156 @@
 from __future__ import unicode_literals, print_function, division
 
 import logging
-import httplib
-from os import abort
-from flask import request, redirect
+from datetime import datetime
+from flask import request, redirect, url_for, jsonify
 from tools.dbi import from_db
 from tools.dbi import transactional
+from tools.mylog import get_logger
 from . import trade_mod as mod
+import zgt.service as zgt
 
 
-log = logging.getLogger(__name__)
+logger = get_logger(__name__)
 
 
-@mod.route('/pay', methods=['POST'])
-def pay_action():
-    data = request.form
-    source_channel = data.get('source_channel', None)
-    source_order_id = data.get('source_order_id', None)
-    source_order_name = data.get('source_order_name', None)
-    payer = data.get('payer', None)
-    amount = float(data.get('amount', -1))
-    if source_channel is None or source_order_id is None or source_order_name is None or amount <= 0:
-        abort(httplib.BAD_REQUEST)
-    trades = from_db().list('select * from pay_trades')
-    trade_id = create_trade_record(source_channel, source_order_id, source_order_name, amount, payer, trade_type=1)
-    print(trade_id)
+@mod.route('/pay/', methods=['GET', 'POST'])
+def pay():
+    """支付接口
+    :return:
+    """
+    # TODO: 加密接口数据
+    data = request.args
+    if request.method == 'POST':
+        data = request.form
 
-    print(trades)
-    return redirect(get_payment_agent_url())
+    source_channel = zgt.format_string('source_channel')
+    source_order_id = zgt.format_string('source_order_id')
+    source_user_id = zgt.format_string(data.get('source_user_id'))
+    source_account = zgt.format_string(data.get('source_account'))
+    amount = float(zgt.format_string(data.get('amount', '0')))
+    to_account = zgt.format_string(data.get('to_account'))
+    product_name = zgt.format_string(data.get('product_name'))
+    product_cat = zgt.format_string(data.get('product_cat'))
+    product_desc = zgt.format_string(data.get('product_desc'))
+    trade_type = 1
+    trade_serial_no = "ZGTPAY" + datetime.now().strftime("%y%m%d_%H%M%S%f")
+    status = "INIT"
+
+    # TODO: check parameters.
+    # requires, account exists.
+
+    record = {'source_channel': source_channel, 'source_order_id': source_order_id,
+              'source_user_id': source_user_id, 'source_account': source_account,
+              'amount': amount, 'to_account': to_account,
+              'trade_type': trade_type, 'trade_serial_no': trade_serial_no, 'status': status,
+              'product_name': product_name, 'product_cat': product_cat, 'product_desc': product_desc}
+    trade_id = create_trade_record(record)
+    trade_record = from_db().get('select * from trade_records where id=%(id)s', id=trade_id)
+
+    # TODO: 配置此地址
+    host_url = request.host_url.strip('/')
+    pay_callback_url = host_url + url_for('trade.pay_callback')
+    pay_web_callback_url = host_url + url_for('trade.pay_web_callback')
+
+    request_params = {
+        'requestid': trade_record['trade_serial_no'],
+        'amount': str(trade_record['amount']),
+        'assure': '0',
+        'productname': trade_record['product_name'],
+        'productcat': trade_record['product_cat'],
+        'productdesc': trade_record['product_desc'],
+        'divideinfo': '',
+        'callbackurl': pay_callback_url,
+        'webcallbackurl': pay_web_callback_url,
+        'bankid': '',
+        'period': '',
+        'memo': '',
+        'payproducttype': 'ONEKEY',
+        'userno': gen_userno(trade_record['source_account'], trade_record['source_user_id']),
+        'ip': '',
+        'cardname': '',
+        'idcard': '',
+        'bankcardnum': ''
+    }
+
+    request_result = zgt.payment_request(request_params)
+
+    code = zgt.format_string(request_result.get('code'))
+    msg = zgt.format_string(request_result.get('msg'))
+    custom_error = zgt.format_string(request_result.get('customError'))
+    payurl = zgt.format_string(request_result.get('payurl'))
+
+    # TODO: 跳转到错误页面
+    if custom_error != "":
+        return jsonify(customError=custom_error)
+    elif code != "1":
+        return jsonify(code=code, msg=msg)
+
+    # 成功
+    if payurl != "":
+        return redirect(payurl)
+
+    # 无卡直连成功
+    # 跳转到支付成功页面
+    # TODO: 添加相应参数
+    return redirect(url_for("main.pay_web_callback", param=1))
+
+
+def gen_userno(source_account, from_user):
+    import hashlib
+    md = hashlib.md5(source_account+from_user)
+    return md.hexdigest()[:30]
 
 
 @transactional
-def create_trade_record(source_channel, source_order_id, source_order_name, amount, payer, trade_type):
-    return from_db().insert('pay_trades', returns_id=True,
-                            source_channel=source_channel,
-                            source_order_id=source_order_id,
-                            source_order_name=source_order_name,
-                            amount=amount,
-                            payer=payer,
-                            trade_type=trade_type
-                            )
+def create_trade_record(record):
+    return from_db().insert('pay_trade_records', returns_id=True, **record)
 
 
+@mod.route('/pay_callback/', methods=['GET', 'POST'])
+def pay_callback():
+    """付款后台回调
+    :return:
+    """
+    data = request.args
+    if request.method == 'POST':
+        data = request.args.form
+    data = data.get('data')
 
-def get_payment_agent_url():
-    return 'http://www.baidu.com'
+    hmac_encryption_order = ['customernumber', 'requestid', 'code', 'notifytype', 'externalid', 'amount',
+                             'cardno', 'bankcode']
+    result = zgt.parse_data_from_yeepay(data, hmac_encryption_order)
+
+    code = result['code']
+    logger.info(str(result))
+    if code == "1":
+        # success
+        trade_serial_no = result['requestid']
+        trade_record = from_db().get("select * from trade_records where trade_serial_no=%(trade_serial_no)s",
+                                     trade_serial_no=trade_serial_no)
+        if trade_record:
+            trade_record = {k: v for k, v in trade_record.items() if k != 'id'}
+            trade_record['status'] = "SUCCESS"
+
+            trade_id = create_trade_record(trade_record)
+        return "SUCCESS"
+    return ""
+
+
+@mod.route('/pay_web_callback/', methods=['GET', 'POST'])
+def pay_web_callback():
+    """付款前台回调
+    :return:
+    """
+    data = request.args.get('data')
+    if request.method == 'POST':
+        data = request.args.form
+    data = data.get('data')
+
+    result = {}
+    if data:
+        hmac_encryption_order = ['customernumber', 'requestid', 'code', 'notifytype', 'externalid', 'amount',
+                                 'cardno', 'bankcode']
+        result = zgt.parse_data_from_yeepay(data, hmac_encryption_order)
+
+    return jsonify(result)

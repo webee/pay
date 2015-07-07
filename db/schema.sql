314f8f198f5605fdbdef66e0373d2c2f291656ea
yiwang <yiwang@lvye.com>
2015-07-07 13:52:28 +0800

    add table payments, accounts.

--- db/schema.sql
+++ db/schema.sql
@@ -4,36 +4,37 @@ CREATE TABLE db_migration (
 
 insert into db_migration values(0);
 
-create table trade_types(
-  id int PRIMARY KEY ,
-  name VARCHAR(30)
-) ;
-insert into trade_types values(1, '支付');
-insert into trade_types values(2, '提现');
-insert into trade_types values(3, '退款');
 
-
-CREATE TABLE trade_records(
+CREATE TABLE payments(
   id int AUTO_INCREMENT PRIMARY KEY ,
-  trade_type int NOT NULL ,
-  trade_serial_no VARCHAR(50) NOT NULL ,
-  source_channel VARCHAR(128) NOT NULL ,
-  source_order_id VARCHAR(60) NOT NULL ,
-  source_user_id VARCHAR(32) NOT NULL ,
-  source_account VARCHAR(32) NOT NULL ,
-  amount DECIMAL(12,2) ,
-  to_account VARCHAR(32) NOT NULL ,
+  order_source VARCHAR(32) NOT NULL ,
+  order_id VARCHAR(64) NOT NULL ,
+
   product_name VARCHAR(50) NOT NULL ,
-  product_cat VARCHAR(50) NOT NULL ,
+  product_category VARCHAR(50) NOT NULL ,
   product_desc VARCHAR(50) NOT NULL ,
-  status VARCHAR(32) NOT NULL ,
+
   created_at TIMESTAMP DEFAULT current_timestamp,
-  FOREIGN KEY pay_trades_type (trade_type) REFERENCES trade_types (id)
+
+  transaction_id VARCHAR(64) NOT NULL ,
+
+  account_id VARCHAR(64) NOT NULL ,
+  to_account_id VARCHAR(64) NOT NULL ,
+  amount DECIMAL(12, 2) NOT NULL ,
+
+  -- nullable
+  yeepay_transaction_id VARCHAR(64) ,
+  success SMALLINT , -- 0/1, FAIL/SUCCESS
+  end_at TIMESTAMP DEFAULT current_timestamp
 );
 
-CREATE TABLE zyt_accounts(
+
+CREATE TABLE accounts(
   id int AUTO_INCREMENT PRIMARY KEY ,
-  account VARCHAR(32) NOT NULL UNIQUE ,
-  password VARCHAR(64) NOT NULL ,
-  pay_password VARCHAR(64)
+  account_id VARCHAR(64) NOT NULL UNIQUE ,
+  password VARCHAR(64),
+  pay_password VARCHAR(64),
+  user_source VARCHAR(32),
+  user_id VARCHAR(64),
+  activated BOOLEAN NOT NULL DEFAULT FALSE COMMENT '是否激活'
 );

e1a873a810ba5dbaf318f14c93780487e5e72175
rainedream <raineddream@gmail.com>
2015-07-07 18:17:21 +0800

    Open page to register bank card

--- src/pay_site/account/views.py
+++ src/pay_site/account/views.py
@@ -13,6 +13,14 @@ def list_bankcards(account_id):
     bankcards = bankcard.list_all(account_id)
     return render_template('bankcard_management.html', bankcards=bankcards)
 
+@mod.route('/<int:account_id>/bankcards', methods=['POST'])
+def add_bankcard(account_id):
+    return render_template('new_bankcard.html')
+
+@mod.route('/<int:account_id>/bankcards/new')
+def register_bankcard_info(account_id):
+    return render_template('new_bankcard.html')
+
 @mod.route('/<int:account_id>/withdraw')
 def withdraw(account_id):
     return render_template('withdraw.html')

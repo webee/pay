59d91859da794136aed8fa7f360dcf2ea71bffd3
yiwang <yiwang@lvye.com>
2015-07-08 10:50:11 +0800

    fix test_run.sh.

--- test_run.sh
+++ test_run.sh
@@ -3,10 +3,10 @@
 #set -e
 source $(dirname $0)/scripts/env_conf.sh
 
-root=$(dirname $(${abspath} $0))
+root=$(dirname $0)
 cd ${root}
 source /etc/profile
-source ${root}/env.local.sh
+source env.local.sh
 if [ $? -ne 0 ]; then
     echo "${root}/env.local.sh not exists." >/dev/stderr
     echo "need local env config." >/dev/stderr

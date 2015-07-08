725f2fb122ab6fdffd252746ea62bf7bf60557d6
rainedream <raineddream@gmail.com>
2015-07-08 11:53:53 +0800

    Build api to list bank cards of specific account

--- src/pay_site/account/views.py
+++ src/pay_site/account/views.py
@@ -1,17 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function, division
+import logging
+
 from flask import render_template
 from . import account_mod as mod
-from .services import bankcard
-import logging
 
 log = logging.getLogger(__name__)
 
-
-@mod.route('/<int:account_id>/bankcards')
-def list_bankcards(account_id):
-    bankcards = bankcard.list_all(account_id)
-    return render_template('bankcard_management.html', bankcards=bankcards)
 
 @mod.route('/<int:account_id>/bankcards', methods=['POST'])
 def add_bankcard(account_id):

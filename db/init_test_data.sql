10713c8349132415a0edf36176153d66224f89e2
rainedream <raineddream@gmail.com>
2015-07-13 13:51:18 +0800

    Prepare base data for test env

--- db/init_test_data.sql
+++ db/init_test_data.sql
@@ -1,4 +1,6 @@
-INSERT INTO account(user_source, user_id) VALUES('绿野', '1');
+INSERT INTO client_info(name) VALUES('绿野活动');
+
+INSERT INTO account(user_source, user_id) VALUES('绿野活动帐户', '1');
 
 INSERT INTO virtual_account_bank_card(account_id, card_no, bank_name, bank_account_name, bank_account_type, reserved_phone, province, city)
   VALUES(1, '4219880509175347', '招商银行', '张三', 'Private', '1328760983', '北京', '北京');

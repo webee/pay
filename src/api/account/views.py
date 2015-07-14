6afe00a70547d763064736d246be95c40beb1c87
rainedream <raineddream@gmail.com>
2015-07-14 12:05:45 +0800

    List acount's all bank cards

--- src/api/account/views.py
+++ src/api/account/views.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function, division
+import json
 import logging
 
+import bankcard
 from . import account_mod as mod
 from flask import jsonify
 
 log = logging.getLogger(__name__)
 
 
-@mod.route('/<account_id>/withdraw', methods=['POST'])
+@mod.route('/<int:account_id>/withdraw', methods=['POST'])
 def withdraw(account_id):
     return jsonify({})
+
+
+@mod.route('/<int:account_id>/bankcards', methods=['GET'])
+def list_all(account_id):
+    bankcards = bankcard.list_all(account_id)
+    return json.dumps(bankcards), 200

0ffcc07b536a0b5cd575e403b2f5a01e9675733d
rainedream <raineddream@gmail.com>
2015-07-15 19:06:08 +0800

    Remove unused configs

--- src/api/payment/__init__.py
+++ src/api/payment/__init__.py
@@ -1,24 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint
-from api. attr_dict import AttrDict
-from api.base_config import lianlian_base_config
 
 pay_mod = Blueprint('payment', __name__)
-
-lianlian_payment_config = AttrDict(
-    busi_partner=AttrDict(
-        virtual_goods='101001',
-        physical_goods='109001',
-    ),
-
-    default_order_expiration='10080',
-
-    payment=AttrDict(
-        url='https://yintong.com.cn/payment/bankgateway.htm',
-        notify_url='/pay/{uuid}/result',
-        return_url='http://www.baidu.com'
-    )
-)
-config = lianlian_payment_config.merge_to(lianlian_base_config)
 
 from . import views
--- src/api/refund/__init__.py
+++ src/api/refund/__init__.py
@@ -1,16 +1,6 @@
 # -*- coding: utf-8 -*-
-from api.attr_dict import AttrDict
-from api.base_config import lianlian_base_config
 from flask import Blueprint
 
 refund_mod = Blueprint('refund', __name__)
-
-lianlian_refund_config = AttrDict(
-    refund=AttrDict(
-        url='https://yintong.com.cn/traderapi/refund.htm',
-        notify_url='/refund/{uuid}/result',
-    )
-)
-config = lianlian_refund_config.merge_to(lianlian_base_config)
 
 from . import views

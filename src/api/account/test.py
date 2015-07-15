5b0a5422b08e1f7c4c84d9c04e3a47a70eab9ddb
yiwang <yiwang@lvye.com>
2015-07-15 15:14:37 +0800

    add callback_url in withdraw.

--- src/api/account/test.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-from __future__ import unicode_literals, print_function
-from datetime import datetime
-
-
-def get_current_datetime_str():
-    now = datetime.now()
-    return now.strftime('%Y%m%d%H%M%S%f')
-

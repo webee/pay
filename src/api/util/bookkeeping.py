492ce3f7bb3f6f8d1ed1f43264f7b1efd4583e00
rainedream <raineddream@gmail.com>
2015-07-17 14:57:17 +0800

    Refactor book-keeping

--- src/api/util/bookkeeping.py
+++ src/api/util/bookkeeping.py
@@ -15,12 +15,41 @@ ACCOUNTS_SIDES = {
 
 class Event(object):
     def __init__(self, account_id, source_type, step, source_id, amount):
-        self.account_id = account_id,
-        self.source_type = source_type,
-        self.step = step,
-        self.source_id = source_id,
-        self.amount = amount,
-        self.created_on = datetime.now()
+        self.__account_id = account_id,
+        self.__source_type = source_type,
+        self.__step = step,
+        self.__source_id = source_id,
+        self.__amount = amount,
+        self.__created_on = datetime.now()
+
+    @property
+    def account_id(self):
+        return self.__account_id
+
+    @property
+    def source_type(self):
+        return self.__source_type
+
+    @property
+    def step(self):
+        return self.__step
+
+    @property
+    def source_id(self):
+        return self.__source_id
+
+    @property
+    def amount(self):
+        return self.__amount
+
+    @property
+    def created_on(self):
+        return self.__created_on
+
+    @property
+    def items(self):
+        return {'account_id':self.account_id, 'source_type':self.source_type, 'step':self.step,
+                'source_id':self.source_id, 'amount':self.amount, 'created_on':self.created_on}
 
 
 def get_debit_and_credit_both_increased(account_a, account_b):
@@ -58,7 +87,7 @@ def get_debit_and_credit_accounts(increased_accounts, decreased_accounts):
 
 
 def two_accounts_bookkeeping(event, debit_account, credit_account):
-    amount = event['amount']
+    amount = event.amount
     return _debit_credit_bookkeeping(event, ((debit_account, amount),), ((credit_account, amount),))
 
 
@@ -69,18 +98,28 @@ def _debit_credit_bookkeeping(event, debit_items, credit_items):
     :param credit_items: 贷记事项((account, amount), ...)
     :return:
     """
-    amount = event['amount']
-    debits_amount = sum(a for _, a in debit_items)
-    credits_amount = sum(a for _, a in credit_items)
-    if not amount == debits_amount == credits_amount:
+    if not _is_balanced(event.amount, debit_items, credit_items):
         raise ValueError('event, debit and credit amount are not equal.')
 
-    created_on = event['created_on']
-    account_id = event['account_id']
+    created_on = event.created_on
+    account_id = event.account_id
 
-    db = from_db()
-    event_id = db.insert('event', returns_id=True, **event)
+    event_id = _create_event(event)
+    _record_debit(event_id, account_id, debit_items, created_on)
+    _record_credit(event_id, account_id, credit_items, created_on)
 
+
+def _is_balanced(event_amount, debit_items, credit_items):
+    debits_amount = sum(a for _, a in debit_items)
+    credits_amount = sum(a for _, a in credit_items)
+    return event_amount == debits_amount == credits_amount
+
+
+def _create_event(event):
+    return from_db().insert('event', returns_id=True, **event.items)
+
+
+def _record_debit(event_id, account_id, debit_items, created_on):
     for account, amount in debit_items:
         account_log = {
             'event_id': event_id,
@@ -89,8 +128,10 @@ def _debit_credit_bookkeeping(event, debit_items, credit_items):
             'amount': amount,
             'created_on': created_on
         }
-        db.insert(account + '_account_transaction_log', **account_log)
+        from_db().insert(account + '_account_transaction_log', **account_log)
 
+
+def _record_credit(event_id, account_id, credit_items, created_on):
     for account, amount in credit_items:
         account_log = {
             'event_id': event_id,
@@ -99,4 +140,4 @@ def _debit_credit_bookkeeping(event, debit_items, credit_items):
             'amount': amount,
             'created_on': created_on
         }
-        db.insert(account + '_account_transaction_log', **account_log)
+        from_db().insert(account + '_account_transaction_log', **account_log)

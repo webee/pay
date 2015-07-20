dd44ae1c97c2a044daf2a5fa834b2f535ecc6c33
rainedream <raineddream@gmail.com>
2015-07-20 11:22:47 +0800

    Etract duplicated code into a method

--- src/api/util/response.py
+++ src/api/util/response.py
@@ -3,24 +3,22 @@ from flask import jsonify, abort
 
 
 def created(id):
-    resp = jsonify({'id': id})
-    resp.status_code = 201
-    return resp
+    return _response(201, {'id': id})
 
 
 def accepted(id):
-    resp = jsonify({'id': id})
-    resp.status_code = 202
-    return resp
+    return _response(202, {'id': id})
 
 
 def not_found():
-    resp = jsonify({})
-    resp.status_code = 404
-    return resp
+    return _response(404)
 
 
 def bad_request(message, **request_params):
-    resp = jsonify({'error': message, 'params': str(request_params)})
-    resp.status_code = 400
+    return _response(400, {'error': message, 'params': str(request_params)})
+
+
+def _response(status_code, obj=None):
+    resp = jsonify(obj if obj else {})
+    resp.status_code = status_code
     return resp

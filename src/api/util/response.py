cd3d9477b5d65ec0e3d5120ae6d4bf72cd987592
yiwang <yiwang@lvye.com>
2015-07-21 11:56:18 +0800

    fix datetime format to %Y%m%d %H:%M:%S.%f

--- src/api/util/response.py
+++ src/api/util/response.py
@@ -10,7 +10,7 @@ class _DecimalEncoder(json.JSONEncoder):
         if isinstance(o, decimal.Decimal):
             return float(o)
         if isinstance(o, datetime.datetime):
-            return o.strftime('%Y%m%d %H%M%S.%f')
+            return o.strftime('%Y%m%d %H:%M:%S.%f')
         return super(_DecimalEncoder, self).default(o)
 
 

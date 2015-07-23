76745119386438179a51100f00e211c2658cdb38
yiwang <yiwang@lvye.com>
2015-07-23 09:53:01 +0800

    redirect / to huodong.

--- src/api/index/views.py
+++ src/api/index/views.py
@@ -2,9 +2,10 @@
 from __future__ import unicode_literals, print_function, division
 
 from . import index_mod as mod
-from flask import render_template
+from flask import render_template, redirect
 
 
 @mod.route('/')
 def index():
-    return render_template('index.html')
+    return redirect('http://huodong.lvye.com')
+    # return render_template('index.html')

bdbfef87554693c97da5c39d96420a62116dcdcf
rainedream <raineddream@gmail.com>
2015-07-23 17:49:04 +0800

    Concentrate 'get_amount', 'succeed', 'fail' into payment

--- src/api/payment/payment.py
+++ src/api/payment/payment.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from api.util.enum import enum
 from tools.dbe import from_db
 
 
-PaymentState = enum(CREATED='CREATED')
+PaymentState = enum(CREATED='CREATED', SUCCESS='SUCCESS', FAILED='FAILED')
 
 
 def create(id, client_id, payer_account_id, payee_account_id, order, amount, callback_url,
@@ -31,3 +31,28 @@ def create(id, client_id, payer_account_id, payee_account_id, order, amount, cal
 
 def find(id):
     return from_db().get('SELECT * FROM payment WHERE id = %(id)s', id=id)
+
+
+def get_amount(id):
+    return from_db().get_scalar('SELECT amount FROM payment WHERE id = %(id)s AND success IS NULL', id=id)
+
+
+def succeed(id, paybill_id):
+    now = datetime.now()
+    from_db().execute(
+        """
+            UPDATE payment
+              SET state = %(state)s, transaction_ended_on = %(ended_on)s, paybill_id=%(paybill_id)s,
+                  auto_confirm_expired_on=%(expired_on)s
+              WHERE id = %(id)s
+        """,
+        id=id, state=PaymentState.SUCCESS, paybill_id=paybill_id, ended_on=now, expired_on=_few_days_later(now, 3))
+
+
+def fail(id):
+    from_db().execute('UPDATE payment SET state = %(state)s, transaction_ended_on = %(ended_on)s WHERE id = %(id)s',
+                      id=id, state=PaymentState.FAILED, ended_on=datetime.now())
+
+
+def _few_days_later(from_datetime, days):
+    return from_datetime + timedelta(days=days)
--- src/api/payment/postpay.py
+++ src/api/payment/postpay.py
@@ -1,18 +1,15 @@
-from datetime import datetime, timedelta
-
+from . import payment
 from api.constant import SourceType, PayStep
 from api.util.bookkeeping import bookkeeping, Event
 from api.util.uuid import decode_uuid
-from tools.dbe import from_db, transactional
+from tools.dbe import transactional
 
 
-def is_valid_payment(transaction_id, uuid, paid_amount):
-    if transaction_id != decode_uuid(uuid):
+def is_valid_payment(payment_id, uuid, paid_amount):
+    if payment_id != decode_uuid(uuid):
         return False
 
-    amount = from_db().get_scalar('SELECT amount FROM payment WHERE id = %(id)s AND success IS NULL',
-                                  id=transaction_id)
-    return amount == paid_amount
+    return payment.get_amount(payment_id) == paid_amount
 
 
 def is_successful_payment(pay_result):
@@ -20,42 +17,21 @@ def is_successful_payment(pay_result):
 
 
 @transactional
-def fail_payment(transaction_id):
-    from_db().execute('UPDATE payment SET success = 0, transaction_ended_on = %(ended_on)s WHERE id = %(id)s',
-                      id=transaction_id, ended_on=datetime.now())
+def fail_payment(payment_id):
+    payment.fail(payment_id)
 
 
 @transactional
 def succeed_payment(payment_id, paybill_id):
-    _update_payment(payment_id, paybill_id)
+    payment.succeed(payment_id, paybill_id)
 
-    payment = _find_payment(payment_id)
+    pay_record = payment.find(payment_id)
     bookkeeping(
-        Event(payment['payee_account_id'], SourceType.PAY, PayStep.SECURED, payment_id, payment['amount']),
+        Event(pay_record['payee_account_id'], SourceType.PAY, PayStep.SECURED, payment_id, pay_record['amount']),
         '+secured', '+asset'
     )
 
 
 def get_payment_success_callback_interface_on_client(uuid):
     payment_id = decode_uuid(uuid)
-    return from_db().get_scalar('SELECT client_success_return_url FROM payment WHERE id=%(id)s', id=payment_id)
-
-
-def _find_payment(transaction_id):
-    return from_db().get('SELECT payee_account_id, amount FROM payment WHERE id=%(id)s', id=transaction_id)
-
-
-def _update_payment(transaction_id, paybill_id):
-    now = datetime.now()
-    from_db().execute(
-        """
-            UPDATE payment
-              SET success = 1, transaction_ended_on = %(ended_on)s, paybill_id=%(paybill_id)s,
-                  auto_confirm_expired_on=%(expired_on)s
-              WHERE id = %(id)s
-        """,
-        id=transaction_id, paybill_id=paybill_id, ended_on=now, expired_on=_few_days_later(now, 3))
-
-
-def _few_days_later(from_datetime, days):
-    return from_datetime + timedelta(days=days)
+    return payment.find(payment_id)['client_success_return_url']

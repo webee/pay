d4f7297595b5c34de4a870ddeae62535d9955fb2
rainedream <raineddream@gmail.com>
2015-07-25 16:25:17 +0800

    Remove unused codes

--- src/site_manager.py
+++ src/site_manager.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# coding=utf-8
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function
 
 from flask.ext.script import Manager, Server
@@ -8,11 +8,6 @@ from website import create_app
 
 manager = Manager(create_app)
 manager.add_option('-e', '--env', dest='env', required=False)
-
-
-# def make_shell_context():
-#     return dict(app=app)
-# manager.add_command("shell", Shell(make_context=make_shell_context))
 
 server = Server(host="0.0.0.0", port=5001)
 manager.add_command("runserver", server)

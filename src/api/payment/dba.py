1ba68031edba7eb279ce2ecece770b8f2d4c6c57
yiwang <yiwang@lvye.com>
2015-07-28 13:25:55 +0800

    fix field error.

--- src/api/payment/dba.py
+++ src/api/payment/dba.py
@@ -16,6 +16,6 @@ def set_refunded_amount(db, id, amount):
 def transit_state(db, id, prev_state, new_state):
     return db.execute("""
                     UPDATE payment
-                      SET state=%(new_state)s, updated=%(updated)s
+                      SET state=%(new_state)s, updated_on=%(updated_on)s
                       WHERE id=%(id)s AND state=%(prev_state)s
-        """, id=id, prev_state=prev_state, new_state=new_state, updated=datetime.now()) > 0
+        """, id=id, prev_state=prev_state, new_state=new_state, updated_on=datetime.now()) > 0

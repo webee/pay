b89081391c53960a4925a5aba3e73da7109259b2
rainedream <raineddream@gmail.com>
2015-07-29 11:02:35 +0800

    Get account through client_id & user_id

--- src/api/client/views.py
+++ src/api/client/views.py
@@ -4,18 +4,16 @@ import logging
 
 from api.util import response
 from . import client_mod as mod
-from api.account.account import get_cash_balance
 from api.account.account.dba import get_account
 
 
 log = logging.getLogger(__name__)
 
 
-@mod.route('/<int:client_id>/users/<user_id>/balance', methods=['GET'])
-def get_balance(client_id, user_id):
+@mod.route('/<int:client_id>/users/<user_id>/account', methods=['GET'])
+def get_account(client_id, user_id):
     account = get_account(client_id, user_id)
     if not account:
         response.not_found({'client_id':client_id, 'user_id':user_id})
 
-    balance = get_cash_balance(account['id'])
-    return response.ok(balance=balance)
+    return response.ok(account_id=account['id'])

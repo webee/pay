4c0fbb208e4f9cfbd6c1fb1f8705dc0a222f1680
rainedream <raineddream@gmail.com>
2015-07-29 17:36:43 +0800

    Fix package import error

--- src/api/client/views.py
+++ src/api/client/views.py
@@ -4,6 +4,7 @@ import logging
 
 from api.util import response
 from . import client_mod as mod
+from api.account.account import get_cash_balance
 from api.account.account.dba import get_account
 
 
@@ -17,5 +18,5 @@ def get_account_info(client_id, user_id):
         response.not_found({'client_id': client_id, 'user_id': user_id})
 
     account_id = account['id']
-    balance = account.get_cash_balance(account_id)
+    balance = get_cash_balance(account_id)
     return response.ok(account_id=account_id, balance=balance)

af94f49ece538a58f56d743ffc407deb7311fdcd
rainedream <raineddream@gmail.com>
2015-07-31 11:29:23 +0800

    Redirect to official host when access api root

--- src/api/index/__init__.py
+++ src/api/index/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint
 
-index_mod = Blueprint('main', __name__, template_folder='./templates', static_folder='static')
+index_mod = Blueprint('main', __name__)
 
 from . import views
--- src/api/index/views.py
+++ src/api/index/views.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function, division
+from flask import redirect
 
 from . import index_mod as mod
-from flask import render_template, redirect
 
 
 @mod.route('/')
 def index():
+    return _redirect_to_official_host
+
+
+def _redirect_to_official_host():
     return redirect('http://huodong.lvye.com')
-    # return render_template('index.html')

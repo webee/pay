854c6167778e6a5f028f8dbd4cec2d571bcbbd69
rainedream <raineddream@gmail.com>
2015-08-01 11:20:51 +0800

    Remove invalid import

--- src/tools/hash.py
+++ src/tools/hash.py
@@ -3,18 +3,18 @@ from __future__ import unicode_literals, print_function, division
 import hashlib
 import hmac
 from encoding import *
-from top_config import config
 
 
-def get_password_hash(password):
-    return get_hmac(password, strong=True)
+def hash_password(password):
+    return _get_hmac(password, strong=True)
 
 
-def get_hmac(*parts, **kwargs):
+def _get_hmac(*parts, **kwargs):
     strong = kwargs.pop('strong', True)
     digestmod = hashlib.sha256 if strong else hashlib.sha1
     msg = '|'.join([unicode(part) for part in parts])
-    return hmac.new(to_str(get_hash_salt()), to_str(msg), digestmod).hexdigest()
+    return hmac.new(to_str(_get_hash_salt()), to_str(msg), digestmod).hexdigest()
 
-def get_hash_salt():
-    return config().get('app','hash_salt')
+
+def _get_hash_salt():
+    return 'lvye'

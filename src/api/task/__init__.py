e0eb603acdb7eede241c8a68ec074a11ccd83217
rainedream <raineddream@gmail.com>
2015-08-01 18:03:35 +0800

    Remove config injection in celery task

--- src/api/task/__init__.py
+++ src/api/task/__init__.py
@@ -3,9 +3,5 @@ import os
 from celery import Celery
 
 
-def make_celery_app(name, config):
-    app = Celery(name)
-    app.config_from_object(config)
-    app.conf.update(os.environ)
-
-    return app
+def make_celery_app(name):
+    return Celery(name)
--- src/api/task/pay_tasks.py
+++ src/api/task/pay_tasks.py
@@ -6,10 +6,9 @@ from api.payment.confirm_pay import list_all_expired_payments
 from api.payment.confirm_pay import confirm_payment_by_id
 from api.account.account.dba import list_users_with_unsettled_cash
 from api.account.account.balance import settle_user_account_balance
-from top_config import api_task_celery as config
 
 logger = get_logger(__name__)
-app = make_celery_app('pay', config)
+app = make_celery_app('pay')
 
 
 @app.task

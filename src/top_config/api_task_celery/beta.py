1a93af7455c9e3096e94f1b10dab01d84a76e788
rainedream <raineddream@gmail.com>
2015-08-01 18:05:34 +0800

    Remove top_config

--- src/top_config/api_task_celery/beta.py
+++
@@ -1,3 +0,0 @@
-# coding=utf-8
-BROKER_URL = 'amqp://lvye_pay:p@55word@192.168.12.126:5672/lvye_pay'
-CELERY_RESULT_BACKEND = 'amqp://lvye_pay:p@55word@192.168.12.126:5672/lvye_pay'
--- src/top_config/db/__init__.py
+++
@@ -1,6 +0,0 @@
-# coding=utf-8
-USERNAME = 'lvye_pay'
-PASSWORD = 'p@55word'
-HOST = 'localhost'
-PORT = 3306
-INSTANCE = 'lvye_pay'

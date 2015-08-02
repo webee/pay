c11e8df98de68ac07e1158edd9df08f02c5a1115
yiwang <yiwang@lvye.com>
2015-08-02 22:00:37 +0800

    fix typo.

--- src/api/refund/transit.py
+++ src/api/refund/transit.py
@@ -10,18 +10,18 @@ from . import event
 def refund_frozen(db, refund_order):
     if payment_transit.refund_started(db, refund_order.payment_id):
         if dba.transit_state(db, refund_order.id, RefundState.FROZEN, RefundState.FROZEN):
-            return event.frozen(refund_order.payee_acount_id, refund_order.id, refund_order.amount)
+            return event.frozen(refund_order.payee_account_id, refund_order.id, refund_order.amount)
 
 
 @db_transactional
 def refund_failed(db, refund_order):
     if payment_transit.refund_ended(db, refund_order.payment_id):
         if dba.transit_state(db, refund_order.id, RefundState.FROZEN, RefundState.FAILED):
-            return event.unfrozen_back(refund_order.payee_acount_id, refund_order.id, refund_order.amount)
+            return event.unfrozen_back(refund_order.payee_account_id, refund_order.id, refund_order.amount)
 
 
 @db_transactional
 def refund_success(db, refund_order):
     if payment_transit.refund_ended(db, refund_order.payment_id, refund_order.amount):
         if dba.transit_state(db, refund_order.id, RefundState.FROZEN, RefundState.SUCCESS):
-            return event.unfrozen_out(refund_order.payee_acount_id, refund_order.id, refund_order.amount)
+            return event.unfrozen_out(refund_order.payee_account_id, refund_order.id, refund_order.amount)

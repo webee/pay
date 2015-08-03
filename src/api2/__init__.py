45336ff5fd04ab33667abbd9960f44296df8cacc
rainedream <raineddream@gmail.com>
2015-08-03 17:16:09 +0800

    Build individual views only for 'FACE' api

--- src/api2/__init__.py
+++ src/api2/__init__.py
@@ -1,24 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function, division
-import os
-
 from flask import Flask
+import os
 
 
 def register_mods(app):
-    from api.index import index_mod
-    from api.payment import pay_mod
-    from api.refund import refund_mod
-    from api.bankcard import card_mod
-    from api.account import account_mod
-    from api.client import client_mod
+    from api2.face_views import mod
 
-    app.register_blueprint(index_mod)
-    app.register_blueprint(pay_mod)
-    app.register_blueprint(refund_mod, url_prefix='/refund')
-    app.register_blueprint(card_mod, url_prefix='/bankcards')
-    app.register_blueprint(account_mod, url_prefix='/accounts')
-    app.register_blueprint(client_mod, url_prefix='/clients')
+    app.register_blueprint(mod)
 
 
 def create_app(env):

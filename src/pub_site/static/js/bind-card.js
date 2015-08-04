975112503c87073f14b846fe6a40f9b126291a52
swzhou <swing.zhou@hotmail.com>
2015-08-05 07:56:18 +0800

    handle csrf globally

--- src/pub_site/static/js/bind-card.js
+++ src/pub_site/static/js/bind-card.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
-    $('.hqyzm').click(function() {
+$(document).ready(function () {
+    $('.hqyzm').click(function () {
         $.ajax('/withdraw/generate-identifying-code', {
             type: 'post',
-            headers: {'X-CSRF-Token': $('#csrf_token').val()},
-            success: function(data, textStatus) {
-                if(textStatus === 'success') {
-                    $('.identifying-code.warn').html('验证码已发送至注册领队时所留手机号');
-                }
+            success: function (data, textStatus) {
+                var message = textStatus === 'success'
+                    ? '验证码已发送至注册领队时所留手机号'
+                    : '验证码发送失败，请重试。';
+                $('.identifying-code.warn').html(message);
             }
         });
     });

3448b200cb0f1c9a712c8f062b1e4f248e299218
rainedream <raineddream@gmail.com>
2015-08-04 11:04:03 +0800

    Rename file

--- src/api2/core/__init__.py
+++ src/api2/core/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from .pay import pay as _pay
 from .transfer import transfer as _transfer
-from .bankcard_bin import query_bankcard_bin as _query_bankcard_bin
+from .bankcard import query_bankcard_bin as _query_bankcard_bin
 
 
 def pay(trade_id, payer_account_id, payer_created_on, payee_account_id, request_from_ip, product_name, product_desc,

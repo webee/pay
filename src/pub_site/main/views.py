febc92951ce3a1e22743853abc54bed36c9446cc
yiwang <yiwang@lvye.com>
2015-08-04 16:46:57 +0800

    update layout.

--- src/pub_site/main/views.py
+++ src/pub_site/main/views.py
@@ -1,8 +1,13 @@
 # coding=utf-8
 from __future__ import unicode_literals
-from flask import request, render_template
+from flask import request, render_template, g
 from flask.ext.login import login_required
 from . import main_mod as mod
+
+
+@mod.before_request
+def set_current_channel():
+    g.current_channel = 'main'
 
 
 @mod.route('/', methods=['GET'])

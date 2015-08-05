ff88e102d054176198a02286e31ab3f896a676f2
rainedream <raineddream@gmail.com>
2015-08-05 13:24:31 +0800

    Change website to post data to api2

--- src/website/sample/pay.py
+++ src/website/sample/pay.py
@@ -6,7 +6,7 @@ from pytoolbox import config
 from website.util.uuid import encode_uuid
 
 
-_API_HOST = config.get('hosts', 'api_gateway')
+_API_HOST = 'http://localhost:5100' or config.get('hosts', 'api_gateway')
 _SITE_HOST = config.get('hosts', 'site_gateway')
 
 
@@ -16,12 +16,14 @@ def pay(amount):
         'client_id': 1,
         'payer': 2001,
         'payee': 1001,
+        'activity_id': '200001',
         'order_no': order_no,
         'order_name': 'Christmas gift',
         'order_desc': 'Gift to my friend',
         'ordered_on': datetime(2015, 6, 19, 18, 28, 35),
         'amount': amount,
-        'client_callback_url': '{0}/pay/{1}/notify-result'.format(_SITE_HOST, order_no)
+        'client_callback_url': '{0}/pay/{1}/notify-result'.format(_SITE_HOST, order_no),
+        'client_async_callback_url': '{0}/pay/{1}/notify-result'.format(_SITE_HOST, order_no)
     }
     return requests.post('{0}/pre-pay'.format(_API_HOST), data=params)
 

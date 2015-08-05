c48238b5d72999f06daa850523249e6a78dcc3d6
rainedream <raineddream@gmail.com>
2015-08-05 11:24:12 +0800

    Grant privileges to user@'localhost'

--- db/init_db.sql
+++ db/init_db.sql
@@ -5,4 +5,5 @@ drop user lvye_pay;
 create user lvye_pay identified by 'p@55word';
 update mysql.user set password=password('p@55word') where user='lvye_pay' or user='lvye_pay@localhost';
 grant all privileges on lvye_pay.* to 'lvye_pay'@'%';
+grant all privileges on lvye_pay.* to 'lvye_pay'@'localhost';
 flush privileges;

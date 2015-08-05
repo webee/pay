5d136a8f3e282bc22d5e6e5946c2990eac942467
yiwang <yiwang@lvye.com>
2015-08-05 16:20:00 +0800

    fix sql.

--- src/api/account/trading/dba.py
+++ src/api/account/trading/dba.py
@@ -7,5 +7,5 @@ def get_cash_events(db, account_id):
     return db.list("""
         SELECT * FROM cash_account_transaction_log c LEFT JOIN event e ON c.event_id=e.id
           WHERE c.account_id=%(account_id)s
-          ORDER BY id DESC
+          ORDER BY c.id DESC
     """, account_id=account_id)

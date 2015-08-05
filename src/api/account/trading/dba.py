7bd4501485c2deba66d7a7029e01eb98f65f504f
yiwang <yiwang@lvye.com>
2015-08-05 16:16:28 +0800

    add trading api.

--- src/api/account/trading/dba.py
+++ src/api/account/trading/dba.py
@@ -3,10 +3,8 @@ from pytoolbox.util import dbe
 
 
 @dbe.db_context
-def get_account_events(db, account_id):
+def get_cash_events(db, account_id):
     return db.list("""
-        SELECT e.* FROM
-                    (SELECT MAX(id) AS id FROM event WHERE account_id=%(account_id)s GROUP by source_type, source_id)s
-                LEFT JOIN event e ON s.id = e.id
-                ORDER BY id DESC;
+        SELECT * FROM cash_account_transaction_log c LEFT JOIN event e ON c.event_id=e.id ORDER BY id DESC
+        WHERE c.account_id=%(account_id)s
     """, account_id=account_id)
--- src/api/account/trading_views.py
+++ src/api/account/trading_views.py
@@ -3,10 +3,14 @@ from __future__ import unicode_literals, print_function, division
 
 from . import account_mod as mod
 from tools.mylog import get_logger
+from .trading import dba
+from api.util import response
 
 logger = get_logger(__name__)
 
 
-@mod.route('/<int:account_id>/trading_records', methods=['POST'])
+@mod.route('/<int:account_id>/cash_events', methods=['GET'])
 def trading_records(account_id):
-    pass
+    cash_events = dba.get_cash_events(account_id)
+
+    return response.list_data(cash_events)

d7ac5b69144e47d52ca85b3f2cc2989519e577fb
yiwang <yiwang@lvye.com>
2015-08-06 11:31:50 +0800

    fix withdraw notify, add bookkeeping.

--- src/api/core/withdraw.py
+++ src/api/core/withdraw.py
@@ -115,8 +115,8 @@ def _request_withdraw(withdraw_id, amount, bankcard, notify_url):
 @transactional
 def _fail_withdraw(db, withdraw_id):
     withdraw_record = get_withdraw_by_id(withdraw_id)
-    _unfreeze_withdraw(db, withdraw_id, withdraw_record['account_id'], withdraw_record['amount'])
-    transit_withdraw_state(db, withdraw_id, WITHDRAW_STATE.FROZEN, WITHDRAW_STATE.SUCCESS)
+    _unfreeze_back_withdraw(db, withdraw_id, withdraw_record['account_id'], withdraw_record['amount'])
+    transit_withdraw_state(db, withdraw_id, WITHDRAW_STATE.FROZEN, WITHDRAW_STATE.FAILED)
 
 
 def _freeze_withdraw(db, withdraw_id, account_id, amount):
@@ -126,11 +126,18 @@ def _freeze_withdraw(db, withdraw_id, account_id, amount):
              (account_id, '+frozen'))
 
 
-def _unfreeze_withdraw(db, withdraw_id, account_id, amount):
+def _unfreeze_back_withdraw(db, withdraw_id, account_id, amount):
     bookkeep(db,
              Event(SourceType.WITHDRAW_FAILED, withdraw_id, amount),
              (account_id, '-frozen'),
              (account_id, '+cash'))
+
+
+def _unfreeze_out_withdraw(db, withdraw_id, account_id, amount):
+    bookkeep(db,
+             Event(SourceType.WITHDRAW_SUCCESS, withdraw_id, amount),
+             (account_id, '-frozen'),
+             (account_id, '-asset'))
 
 
 @transactional
@@ -139,11 +146,15 @@ def _process_withdraw_result(db, withdraw_id, paybill_id, result, failure_info):
     _logger.warn("Withdraw notify result: [{0}], id=[{1}]".format(result, withdraw_id))
 
     if result == _PAY_TO_BANKCARD_RESULT_FAILURE:
+        withdraw_record = get_withdraw_by_id(withdraw_id)
         transit_withdraw_state(db, withdraw_id, WITHDRAW_STATE.FROZEN, WITHDRAW_STATE.FAILED)
+        _unfreeze_back_withdraw(db, withdraw_id, withdraw_record['account_id'], withdraw_record['amount'])
         return True
 
     if result == _PAY_TO_BANKCARD_RESULT_SUCCESS:
+        withdraw_record = get_withdraw_by_id(withdraw_id)
         transit_withdraw_state(db, withdraw_id, WITHDRAW_STATE.FROZEN, WITHDRAW_STATE.SUCCESS)
+        _unfreeze_out_withdraw(db, withdraw_id, withdraw_record['account_id'], withdraw_record['amount'])
         return True
 
     return False

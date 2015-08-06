8953b69aa6f900deeb71b5d578bc63c507d8b1ec
rainedream <raineddream@gmail.com>
2015-08-06 18:01:44 +0800

    Secured account info should be initialized at first

--- db/data_zyt_account.sql
+++ db/data_zyt_account.sql
@@ -1,6 +1,7 @@
-INSERT INTO user_domain(id, name) VALUES(1, '绿野用户中心');
-INSERT INTO user_domain(id, name) VALUES(2, '担保用户中心');
+INSERT INTO user_domain(id, name) VALUES(1, '担保用户中心');
+INSERT INTO user_domain(id, name) VALUES(2, '绿野用户中心');
 
-INSERT INTO channel(id, user_domain_id, name) VALUES(1, 1, '绿叶活动');
+INSERT INTO account(user_domain_id, user_id, info) VALUES(1, 1, '担保账户');
 
-INSERT INTO account(user_domain_id, user_id, info) VALUES(2, 1, '担保账户');
+INSERT INTO channel(id, user_domain_id, name) VALUES(1, 2, '绿叶活动');
+

e141012128fe8e0d7b866cfc2cc1907351ee4202
yiwang <yiwang@lvye.com>
2015-08-10 18:21:01 +0800

    fix config.

--- pub_site/src/pub_site/config/qa.py
+++ pub_site/src/pub_site/config/qa.py
@@ -4,7 +4,9 @@
 class App:
     TESTING = True
 
+
 class PayAPI:
-    ROOT_URL = "http://192.168.12.126:5000/"
+    ROOT_URL = "http://192.168.12.126:5000"
+
 
 HOST_URL = 'http://test_pay.lvye.com:5002'

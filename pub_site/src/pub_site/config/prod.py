384a3c7e1a01009d9ad6a163d7b4a9c0482f122c
swzhou <swing.zhou@hotmail.com>
2015-08-11 22:15:45 +0800

    correct pay amount validation

--- pub_site/src/pub_site/config/prod.py
+++ pub_site/src/pub_site/config/prod.py
@@ -1,11 +1,14 @@
 # coding=utf-8
 import os
 
+
 class App:
     DEBUG = False
     TESTING = False
 
+
 HOST_URL = 'http://pay.lvye.com'
+
 
 class DataBase:
     HOST = os.environ['PAY_PUB_SITE_DATABASE_HOST']
@@ -13,8 +16,10 @@ class DataBase:
     USERNAME = 'lvye_pay'
     PASSWORD = os.environ['PAY_PUB_SITE_DATABASE_PASSWORD']
 
+
 class PayAPI:
     ROOT_URL = 'http://' + os.environ['PAY_API_SITE']
+
 
 class Services:
     LEADER_SERVER = "http://leader.lvye.com"
--- pub_site/src/pub_site/pay/forms.py
+++ pub_site/src/pub_site/pay/forms.py
@@ -8,16 +8,16 @@ from ..constant import PayType
 
 
 def amount_less_than_balance(form, field):
-    result = PayClient().get_balance()
-    if result['status_code'] == 200:
-        balance = result['data']['balance']
-        if float(field.data) > balance:
-            raise StopValidation(u"余额不足")
+    if form.pay_type.data == PayType.BY_BALANCE:
+        result = PayClient().get_balance()
+        if result['status_code'] == 200:
+            balance = result['data']['balance']
+            if float(field.data) > balance:
+                raise StopValidation(u"余额不足")
 
 
 class PayForm(Form):
-    amount = FloatField(u"付款金额(元)", validators=[DataRequired(u"请输入合法金额"), NumberRange(min=0.01, message=u"请输入合法金额"),
-                                                amount_less_than_balance])
+    amount = FloatField(u"付款金额(元)", validators=[DataRequired(u"请输入合法金额"), NumberRange(min=0.01, message=u"请输入合法金额"), amount_less_than_balance])
     pay_type = RadioField(u"选择付款方式", coerce=int,
                           choices=[(PayType.BY_BALANCE, u"账户余额支付"), (PayType.BY_BANKCARD, u"银行卡支付")],
                           default=PayType.BY_BALANCE)

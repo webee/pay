0be7788a7a7a88d62b23558bd12e2fa52b15bcdb
yiwang <yiwang@lvye.com>
2015-08-13 11:55:24 +0800

    add deploy for pub site.

---
+++ pub_site/gunicorn.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+env=${1:-dev}
+host=${2:-127.0.0.1}
+port=${3:-5002}
+
+PROJ_ROOT=$(dirname $0)
+NAME="pay-pub-site"
+FLASK_DIR=${PROJ_ROOT}/src
+VENV_DIR=${PROJ_ROOT}/pub_venv
+NUM_WORKERS=${NUM_WORKERS:-8}
+ENV_FILE=${HOME}/.pay_env.sh
+
+echo "Starting $NAME"
+
+# activate the virtualenv
+source ${VENV_DIR}/bin/activate
+
+export PYTHONPATH=$FLASK_DIR:$PYTHONPATH
+
+# Start your unicorn
+export ENV=${env}
+if [ -f ${ENV_FILE} ]; then
+    source ${ENV_FILE}
+fi
+
+exec gunicorn main:app -b ${host}:${port} \
+  --name $NAME \
+  -k gevent \
+  -w $NUM_WORKERS \
+  --log-level=info \

6a72dc789aa36504af3f6cc03b99f89da50a1751
rainedream <raineddream@gmail.com>
2015-08-13 11:42:58 +0800

    Update state of trade order to be 'RECEIVED' after payment is confirmed

--- api_site/src/api/secured_transaction/payment/confirm_pay.py
+++ api_site/src/api/secured_transaction/payment/confirm_pay.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from ._dba import confirm_to_pay, update_payment_state, PAYMENT_STATE
-from api import core
+from api.core import transfer as core_transfer, update_order_state
 from api.account import get_secured_account_id
 from pytoolbox.util.dbe import transactional
 
@@ -15,8 +15,10 @@ def confirm_payment(pay_record):
 
     confirm_to_pay_request_id = confirm_to_pay(payment_id, payee_account_id, amount)
     trade_info = u'担保支付确认：[{0}] {1}'.format(pay_record['order_id'], pay_record['product_name'])
-    core.transfer(confirm_to_pay_request_id, trade_info, secured_account_id, payee_account_id, amount)
+    core_transfer(confirm_to_pay_request_id, trade_info, secured_account_id, payee_account_id, amount)
 
     update_payment_state(payment_id, PAYMENT_STATE.CONFIRMED)
+
+    update_order_state(payment_id, u'已到账')
 
     return payment_id

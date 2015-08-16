11ea867785eeaf7fe341fcda9520b931f704673c
yiwang <yiwang@lvye.com>
2015-08-17 01:24:47 +0800

    fix.

--- api_site/src/api_x/zyt/entry/payment.py
+++ api_site/src/api_x/zyt/entry/payment.py
@@ -11,22 +11,22 @@ from ..evas import test_pay, lianlian_pay
 def pay(vas_name, tx):
     payment_record = get_payment_by_tx_id(tx.id)
     if vas_name == test_pay.NAME:
-        return _pay_by_test_pay(payment_record)
+        return _pay_by_test_pay(tx, payment_record)
     elif vas_name == lianlian_pay.NAME:
-        return _pay_by_lianlian_pay(payment_record)
+        return _pay_by_lianlian_pay(tx, payment_record)
     return render_template('info.html', title='错误', msg='暂不支持此支付方式')
 
 
-def _pay_by_test_pay(payment_record):
+def _pay_by_test_pay(tx, payment_record):
     from api_x.zyt.evas.test_pay import pay
     return pay(TransactionType.PAYMENT,
-               payment_record.payer_id, payment_record.sn, payment_record.product_name, payment_record.amount)
+               payment_record.payer_id, tx.sn, payment_record.product_name, payment_record.amount)
 
 
-def _pay_by_lianlian_pay(payment_record):
+def _pay_by_lianlian_pay(tx, payment_record):
     from api_x.zyt.evas.lianlian_pay import pay
     from api_x.zyt.vas.user import get_user_by_id
 
     payer = get_user_by_id(payment_record.payer_id)
     return pay(TransactionType.PAYMENT, payer.id, payer.created_on, req.ip(),
-               payment_record.sn, payment_record.product_name, payment_record.product_desc, payment_record.amount)
+               tx.sn, tx.created_on, payment_record.product_name, payment_record.product_desc, payment_record.amount)
--- api_site/src/api_x/zyt/evas/lianlian_pay/__init__.py
+++ api_site/src/api_x/zyt/evas/lianlian_pay/__init__.py
@@ -9,11 +9,11 @@ from .pay import pay as _pay
 NAME = 'LIANLIAN_PAY'
 
 
-def pay(source, user_id, user_created_on, ip, order_no, order_name, order_desc, amount):
+def pay(source, user_id, user_created_on, ip, order_no, ordered_on, order_name, order_desc, amount):
     return_url = lianlian_pay.ROOT_URL + url_for('lianlian_pay_entry.pay_result', pay_source=source)
     notify_url = lianlian_pay.ROOT_URL + url_for('lianlian_pay_entry.pay_notify', pay_source=source)
 
-    return Response(_pay(user_id, user_created_on, ip, order_no, order_name, order_desc, amount,
+    return Response(_pay(user_id, user_created_on, ip, order_no, ordered_on, order_name, order_desc, amount,
                          return_url, notify_url))
 
 

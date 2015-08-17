7983bfd1e1147ac6ffe3292e0f5e7bf5b67500de
yiwang <yiwang@lvye.com>
2015-08-17 14:06:16 +0800

    fix.

--- api_site/src/api_x/zyt/biz/__init__.py
+++ api_site/src/api_x/zyt/biz/__init__.py
@@ -20,7 +20,7 @@ def init_lianlian_pay_notify_handles():
 
     # payment
     register_pay_notify_handle(TransactionType.PAYMENT, NotifyType.Pay.SYNC, handle_payment_result)
-    register_refund_notify_handle(TransactionType.PAYMENT, NotifyType.Pay.ASYNC, handle_payment_notify)
+    register_pay_notify_handle(TransactionType.PAYMENT, NotifyType.Pay.ASYNC, handle_payment_notify)
 
     # refund
     register_refund_notify_handle(TransactionType.REFUND, handle_refund_notify)

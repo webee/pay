3cbe966d4287f34e6f760fdf724f933b66258c4c
yiwang <yiwang@lvye.com>
2015-08-20 14:36:58 +0800

    fix config.

--- pub_site/src/pub_site/config/__init__.py
+++ pub_site/src/pub_site/config/__init__.py
@@ -5,12 +5,19 @@ from pytoolbox import conf
 
 class App:
     ADMINS = ['liufan@lvye.com', 'yiwang@lvye.com', 'mengyu@lvye.com', 'zhoushiwei@lvye.com']
+    JSON_AS_ASCII = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or '.yek eyvl'
-    PROPAGATE_EXCEPTIONS = True
     TESTING = False
     DEBUG = True
     LOGIN_DISABLED = False
     WTF_CSRF_SECRET_KEY = 'a random string'
+
+    SQLALCHEMY_DATABASE_URI = 'mysql://lvye_pay:p@55word@127.0.0.1:3306/lvye_pay'
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_POOL_SIZE = 30
+    SQLALCHEMY_POOL_TIMEOUT = 60
+    SQLALCHEMY_MAX_OVERFLOW = 60
+    SQLALCHEMY_POOL_RECYCLE = 3600
 
 
 class DataBase:
--- pub_site/src/pub_site/config/prod.py
+++ pub_site/src/pub_site/config/prod.py
@@ -6,6 +6,10 @@ class App:
     DEBUG = False
     TESTING = False
 
+    # database
+    SQLALCHEMY_DATABASE_URI = os.environ['PAY_PUB_SITE_DATABASE_URI']
+    SQLALCHEMY_ECHO = False
+
 
 HOST_URL = 'http://pay.lvye.com'
 
@@ -18,9 +22,8 @@ class DataBase:
 
 
 class PayAPI:
-    ROOT_URL = os.environ['PAY_API_SITE']
+    ROOT_URL = os.environ['PAY_API_SITE'] or 'http://pay.lvye.com/api'
 
 
 class Services:
     LEADER_SERVER = "http://leader.lvye.com"
-

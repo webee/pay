b9d58fafa44fc66c44d9cd372a2942590abe1c9d
yiwang <yiwang@lvye.com>
2015-08-20 10:13:20 +0800

    add log notify return.

--- api_site/src/api_x/utils/notify.py
+++ api_site/src/api_x/utils/notify.py
@@ -16,6 +16,7 @@ def notify_client(url, params, methods=['post']):
                 resp = requests.request(method, url, data=params)
                 logger.info("notify url {0} [{1}] status_code [{2}]".format(method, url, resp.status_code))
                 if resp.status_code == 200:
+                    logger.info(resp.content)
                     data = resp.json()
                     if data['code'] == 0:
                         return True

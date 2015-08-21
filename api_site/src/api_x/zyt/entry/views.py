47d0a549d6f2c35fa416eefc1983ae6433b76e2c
yiwang <yiwang@lvye.com>
2015-08-21 10:41:23 +0800

    fix query transation parameter.

--- api_site/src/api_x/zyt/entry/views.py
+++ api_site/src/api_x/zyt/entry/views.py
@@ -10,12 +10,12 @@ from api_x.zyt.biz.transaction import query_user_transactions
 @mod.route('/account_users/<int:account_user_id>/transactions', methods=['GET'])
 def list_transactions(account_user_id):
     data = request.args
-    tx_type = data.get('tx_type')
+    role = data.get('role')
     page_no = int(data.get('page_no', 1))
     page_size = int(data.get('page_size', 20))
     q = data.get('q')
 
-    total_num, utxs = query_user_transactions(account_user_id, tx_type, page_no, page_size, q)
+    total_num, utxs = query_user_transactions(account_user_id, role, page_no, page_size, q)
     txs = [_get_tx(utx) for utx in utxs]
 
     return response.success(total=total_num, transactions=txs)

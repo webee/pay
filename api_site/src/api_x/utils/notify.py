6058cf277ba04ee68c048fff75f01939b93c98f8
yiwang <yiwang@lvye.com>
2015-08-21 10:32:44 +0800

    fix notify.

--- api_site/src/api_x/utils/notify.py
+++ api_site/src/api_x/utils/notify.py
@@ -10,19 +10,21 @@ logger = get_logger(__name__)
 
 def notify_client(url, params, methods=['post']):
     logger.info("notify [{0}]: {1}".format(url, params))
-    if url:
-        for method in methods:
-            try:
-                resp = requests.request(method, url, data=params)
-                logger.info("notify url {0} [{1}] status_code [{2}]".format(method, url, resp.status_code))
-                if resp.status_code == 200:
-                    logger.info('return: {0}'.format(resp.content))
-                    data = resp.json()
-                    if data['code'] in [0, '0']:
-                        return True
-                else:
-                    logger.warning('return {0}'.format(resp.content))
-            except Exception as e:
-                logger.exception(e)
+    if not url:
+        return True
+
+    for method in methods:
+        try:
+            resp = requests.request(method, url, data=params)
+            logger.info("notify url {0} [{1}] status_code [{2}]".format(method, url, resp.status_code))
+            if resp.status_code == 200:
+                logger.info('return: {0}'.format(resp.content))
+                data = resp.json()
+                if data['code'] in [0, '0']:
+                    return True
+            else:
+                logger.warning('return {0}'.format(resp.content))
+        except Exception as e:
+            logger.exception(e)
 
     return False

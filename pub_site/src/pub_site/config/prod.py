8380c71a08fa2c72767dd8acea07a74c6d072f26
yiwang <yiwang@lvye.com>
2015-08-27 17:20:24 +0800

    add keys.

--- pub_site/src/pub_site/config/prod.py
+++ pub_site/src/pub_site/config/prod.py
@@ -16,9 +16,9 @@ HOST_URL = 'http://pay.lvye.com'
 
 
 class PayClientConfig:
-    MD5_KEY = read_string('conf/md5_key.txt')
-    LVYE_PUB_KEY = read_string('conf/lvye_pub_key.txt')
-    CHANNEL_PRI_KEY = read_string('conf/channel_pri_key.txt')
+    MD5_KEY = read_string('conf/keys/md5_key.txt')
+    LVYE_PUB_KEY = read_string('conf/keys/lvye_pub_key.txt')
+    CHANNEL_PRI_KEY = read_string('conf/keys/channel_pri_key.txt')
 
     ROOT_URL = os.environ['PAY_API_SITE'] or 'http://pay.lvye.com/api'
 

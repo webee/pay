dea69a38a00ee73c42450b8800404d09ccad8154
yiwang <yiwang@lvye.com>
2015-08-31 11:49:54 +0800

    add test prefix in lianlian pay user_id.

--- api_site/src/api_x/zyt/entry/payment.py
+++ api_site/src/api_x/zyt/entry/payment.py
@@ -5,6 +5,7 @@ from api_x.zyt.biz.payment import get_payment_by_tx_id
 from flask import render_template
 from api_x.zyt.biz.models import TransactionType
 from api_x.utils import req
+from api_x.config import etc as config
 from ..evas import test_pay, lianlian_pay
 from .. import vas
 
@@ -32,7 +33,7 @@ def _pay_by_lianlian_pay(tx, payment_record):
     from api_x.zyt.user_mapping import get_user_map_by_account_user_id
 
     user_map = get_user_map_by_account_user_id(payment_record.payer_id)
-    user_id = '%s.%d' % (user_map.user_id, user_map.user_domain_id)
+    user_id = '%s%s.%d' % (config.Biz.TX_SN_PREFIX, user_map.user_id, user_map.user_domain_id)
     return pay(TransactionType.PAYMENT, user_id, user_map.created_on, req.ip(),
                tx.sn, tx.created_on, payment_record.product_name, payment_record.product_desc, payment_record.amount)
 

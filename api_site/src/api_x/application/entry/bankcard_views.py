9ac1d9aa396a3f3d9a24dbea49d2c3aa8f37fe21
yiwang <yiwang@lvye.com>
2015-08-31 15:04:05 +0800

    fix.

--- api_site/src/api_x/application/entry/bankcard_views.py
+++ api_site/src/api_x/application/entry/bankcard_views.py
@@ -1,6 +1,5 @@
 # coding=utf-8
 from __future__ import unicode_literals
-from api_x.application.error import BankcardNotFoundError
 from api_x.utils import response
 from api_x.utils.entry_auth import verify_request
 

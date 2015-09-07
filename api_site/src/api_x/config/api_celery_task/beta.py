f1d735959b5362ff553da9dd38e26206149a2512
yiwang <yiwang@lvye.com>
2015-09-07 10:33:20 +0800

    update task beta config.

--- api_site/src/api_x/config/api_celery_task/beta.py
+++ api_site/src/api_x/config/api_celery_task/beta.py
@@ -1,4 +1,16 @@
 # coding=utf-8
+from datetime import timedelta
 
-BROKER_URL = 'amqp://lvye_pay:p@55word@192.168.0.165:5672/lvye_pay'
-CELERY_RESULT_BACKEND = 'amqp://lvye_pay:p@55word@192.168.0.165:5672/lvye_pay'
+BROKER_URL = 'amqp://lvye_pay:p@55word@127.0.0.1:5672/lvye_pay_beta'
+CELERY_RESULT_BACKEND = 'amqp://lvye_pay:p@55word@127.0.0.1:5672/lvye_pay_beta'
+
+
+CELERYBEAT_SCHEDULE = {
+    'test_job-E0.5d': {
+        'task': 'api_x.task.tasks.test_job',
+        'schedule': timedelta(seconds=10),
+        'kwargs': {},
+        'relative': True,
+        'options': {'queue': 'celery_periodic'}
+    },
+}

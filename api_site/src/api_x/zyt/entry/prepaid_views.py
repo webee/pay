621cb9507d0f59fa838ea90a4897425075d5a8fd
yiwang <yiwang@lvye.com>
2015-10-21 02:44:34 +0800

    add check to_user_map.

--- api_site/src/api_x/zyt/entry/prepaid_views.py
+++ api_site/src/api_x/zyt/entry/prepaid_views.py
@@ -27,15 +27,15 @@ def preprepaid():
 
     if not to_domain_name:
         to_user_map = channel.get_user_map(to_user_id)
+        to_domain_name = channel.user_domain.name
     else:
         # 指定不同的to用户域
         to_domain = get_user_domain_by_name(to_domain_name)
         if to_domain is None:
             return response.fail(msg="domain [{0}] not exists.".format(to_domain_name))
         to_user_map = to_domain.get_user_map(to_user_id)
-        if to_user_map is None:
-            return response.fail(msg="to with domain [{0}] user [{1}] not exists.".format(to_domain_name,
-                                                                                          to_user_id))
+    if to_user_map is None:
+        return response.fail(msg="to with domain [{0}] user [{1}] not exists.".format(to_domain_name, to_user_id))
 
     try:
         prepaid_record = create_prepaid(channel, order_id, to_user_map.account_user_id, amount,

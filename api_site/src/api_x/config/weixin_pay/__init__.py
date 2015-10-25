5c5149516a41cc1bc242cf8ff2e90187fd48d426
yiwang <yiwang@lvye.com>
2015-10-26 02:53:16 +0800

    update weixin configs.

--- api_site/src/api_x/config/weixin_pay/__init__.py
+++ api_site/src/api_x/config/weixin_pay/__init__.py
@@ -7,16 +7,15 @@ ROOT_URL = "http://pay.lvye.com/api"
 class AppConfig:
     CONFIGS = {}
 
-    def __init__(self, app_type=None, name=None, is_init=False, config_name='main'):
+    def __init__(self, name='main', app_type=None, is_init=False):
         """
         :param app_type: is_init=True时不能为空
-        :param name: is_init=True时不能为空
+        :param name: is_init=True/False时不能为空
         :param is_init: 是否为初始化
-        :param config_name: is_init=False时不能为空
         :return:
         """
         if not is_init:
-            _config = AppConfig.CONFIGS[config_name]
+            _config = AppConfig.CONFIGS[name]
             self.__dict__ = _config.__dict__
             return
 
@@ -24,21 +23,24 @@ class AppConfig:
         _main = pmc_config.load_yaml('conf/weixin/%s/%s/main.yaml' % (app_type, name))
         _pay_main = pmc_config.load_yaml('conf/weixin/%s/%s/pay/main.yaml' % (app_type, name))
 
+        self.APP_NAME = name
         self.APPID = _main['AppID']
+        self.APP_SECRET = _main['AppSecret']
         self.MCH_ID = _pay_main['MchID']
-        self.API_KEY = pmc_config.read_string('conf/weixin/%s/%s/pay/api_key.txt' % (app_type, name))
         self.API_KEY = pmc_config.read_string('conf/weixin/%s/%s/pay/api_key.txt' % (app_type, name))
         self.CERT_PATH = pmc_config.abstract_path('conf/weixin/%s/%s/pay/appclient_cert.pem' % (app_type, name))
         self.CERT_KEY_PATH = pmc_config.abstract_path('conf/weixin/%s/%s/pay/appclient_key.pem' % (app_type, name))
 
     @staticmethod
-    def init_config(config_name, app_type, name):
-        AppConfig.CONFIGS[config_name] = AppConfig(app_type, name, is_init=True)
+    def init_config(app_type, name, is_main=False):
+        AppConfig.CONFIGS[name] = AppConfig(name, app_type, is_init=True)
+        if is_main:
+            AppConfig.CONFIGS['main'] = AppConfig.CONFIGS[name]
 
 
 # init
-AppConfig.init_config('ilvyewang', 'public_account', 'ilvyewang')
-AppConfig.init_config('lvye_skiing', 'app', 'lvye_skiing')
+AppConfig.init_config('public_account', 'ilvyewang', is_main=True)
+AppConfig.init_config('app', 'lvye_skiing')
 
 
 DEFAULT_ORDER_EXPIRATION_SECONDS = 604800  # 7 days

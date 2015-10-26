7673bc6a460b2cc0e1250f7effe4f15d795c9d21
yiwang <yiwang@lvye.com>
2015-10-26 17:13:35 +0800

    fix.

--- api_site/src/api_x/zyt/evas/weixin_pay/entry/commons.py
+++ api_site/src/api_x/zyt/evas/weixin_pay/entry/commons.py
@@ -15,7 +15,7 @@ def parse_and_verify(f):
         try:
             params = request.view_args
             app = params['app']
-            verified_data = parse_and_verify_request_data(app, request.data)
+            verified_data = parse_and_verify_request_data(request.data, app)
         except ApiError as _:
             return notify_response.wrong()
         request.__dict__['verified_data'] = verified_data

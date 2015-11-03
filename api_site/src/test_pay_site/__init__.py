aa7d5a1d9f4b92ef6a4839cd68dc3a6a6df1aa08
yiwang <yiwang@lvye.com>
2015-11-03 10:02:45 +0800

    use flask utils.

--- api_site/src/test_pay_site/__init__.py
+++ api_site/src/test_pay_site/__init__.py
@@ -4,6 +4,7 @@ import os
 
 from flask import Flask
 from pytoolbox.util import pmc_config
+from pytoolbox.util.flask.utils import ReverseProxied
 
 
 def register_mods(app):
@@ -25,37 +26,3 @@ def create_app(env='dev'):
     # reverse proxied
     app.wsgi_app = ReverseProxied(app.wsgi_app)
     return app
-
-
-class ReverseProxied(object):
-    """Wrap the application in this middleware and configure the
-    front-end server to add these headers, to let you quietly bind
-    this to a URL other than / and to an HTTP scheme that is
-    different than what is used locally.
-
-    In nginx:
-    location /myprefix {
-        proxy_pass http://192.168.0.1:5001;
-        proxy_set_header Host $host;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Scheme $scheme;
-        proxy_set_header X-Script-Name /myprefix;
-        }
-
-    :param app: the WSGI application
-    """
-    def __init__(self, app):
-        self.app = app
-
-    def __call__(self, environ, start_response):
-        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
-        if script_name:
-            environ['SCRIPT_NAME'] = script_name
-            path_info = environ['PATH_INFO']
-            if path_info.startswith(script_name):
-                environ['PATH_INFO'] = path_info[len(script_name):]
-
-        scheme = environ.get('HTTP_X_SCHEME', '')
-        if scheme:
-            environ['wsgi.url_scheme'] = scheme
-        return self.app(environ, start_response)

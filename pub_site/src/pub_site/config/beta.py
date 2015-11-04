84d99bddfdbf17229bfebfada408cfbe99893884
yiwang <yiwang@lvye.com>
2015-11-05 00:18:32 +0800

    fix beta HOST_URL config.

--- pub_site/src/pub_site/config/beta.py
+++ pub_site/src/pub_site/config/beta.py
@@ -5,6 +5,9 @@ class App:
     TESTING = True
 
 
+HOST_URL = 'http://pay.lvye.com/__'
+
+
 class PayClientConfig:
     ROOT_URL = "http://pay.lvye.com/api/__"
     CHECKOUT_URL = 'http://pay.lvye.com/__/checkout/{sn}'

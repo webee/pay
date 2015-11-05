f9a416d7e677661e12489f917d1316224d159552
yiwang <yiwang@lvye.com>
2015-11-05 12:52:28 +0800

    fix weixin vas_name.

--- api_site/src/manager.py
+++ api_site/src/manager.py
@@ -127,13 +127,17 @@ def close_domain_user(domain_name, user_id):
 
 @manager.command
 def query_notify_refund():
+    from api_x.zyt.evas import weixin_pay
     from api_x.zyt.biz.transaction.dba import get_tx_list
     from api_x.constant import TransactionType, RefundTxState
     from api_x.zyt.biz.query_notify import get_query_notify_handle
 
     txs = get_tx_list(TransactionType.REFUND, RefundTxState.CREATED)
     for tx in txs:
-        handle = get_query_notify_handle(tx.type, tx.vas_name)
+        vas_name = tx.vas_name
+        if weixin_pay.is_weixin_pay(tx.vas_name):
+            vas_name = weixin_pay.NAME
+        handle = get_query_notify_handle(tx.type, vas_name)
         res = handle(TransactionType.REFUND, tx.sn, tx.created_on, tx.vas_name, tx.vas_sn)
         print('{0}: {1}'.format(tx.order_id, res))
 

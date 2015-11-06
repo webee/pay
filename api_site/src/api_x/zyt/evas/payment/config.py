fbeba2d79ab213308f7de1fd56774f19f9bb8f92
yiwang <yiwang@lvye.com>
2015-11-06 15:07:22 +0800

    fix, use ordered dict.

--- api_site/src/api_x/zyt/evas/payment/config.py
+++ api_site/src/api_x/zyt/evas/payment/config.py
@@ -1,4 +1,6 @@
 # coding=utf-8
+from __future__ import unicode_literals
+from collections import OrderedDict
 from api_x.config import test_pay, lianlian_pay, weixin_pay
 from api_x.zyt.evas.test_pay import NAME as TEST_PAY
 from api_x.zyt.evas.lianlian_pay import NAME as LIANLIAN_PAY
@@ -24,24 +26,24 @@ PAYMENT_SCENES = {
 
 # 各支付场景与第三方支付的支付方式的关联
 PAYMENT_SCENE_VASE_TYPES = {
-    PaymentScene.WEB: {
+    PaymentScene.WEB: OrderedDict({
         TEST_PAY: test_pay.PaymentType.WEB,
         LIANLIAN_PAY: lianlian_pay.PaymentType.WEB,
         WEIXIN_PAY: weixin_pay.PaymentType.NATIVE,
-    },
-    PaymentScene.WAP: {
+    }),
+    PaymentScene.WAP: OrderedDict({
         TEST_PAY: test_pay.PaymentType.WEB,
         LIANLIAN_PAY: lianlian_pay.PaymentType.WAP,
         #WEIXIN_PAY: weixin_pay.PaymentType.JSAPI,
-    },
-    PaymentScene.WEIXIN: {
+    }),
+    PaymentScene.WEIXIN: OrderedDict({
         TEST_PAY: test_pay.PaymentType.WEB,
         LIANLIAN_PAY: lianlian_pay.PaymentType.WAP,
         #WEIXIN_PAY: weixin_pay.PaymentType.JSAPI,
-    },
-    PaymentScene.lvye_skiing: {
+    }),
+    PaymentScene.lvye_skiing: OrderedDict({
         TEST_PAY: test_pay.PaymentType.APP,
         LIANLIAN_PAY: lianlian_pay.PaymentType.APP,
         WEIXIN_PAY: weixin_pay.PaymentType.APP + '$lvye_skiing',
-    },
+    }),
 }

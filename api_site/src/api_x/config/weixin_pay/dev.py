e6fc0fc0e719efb093543caf0ddcfc4d3b695e8b
yiwang <yiwang@lvye.com>
2015-11-11 11:35:23 +0800

    update weixin_pay config.

--- api_site/src/api_x/config/weixin_pay/dev.py
+++ api_site/src/api_x/config/weixin_pay/dev.py
@@ -1,3 +1,1 @@
 # coding=utf-8
-
-ROOT_URL = "http://dev_pay.lvye.com:5100"
--- api_site/src/api_x/config/weixin_pay/prod.py
+++ api_site/src/api_x/config/weixin_pay/prod.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-ROOT_URL = "http://pay.lvye.com/api/__"
+ROOT_URL = "http://pay.lvye.com/api"
 
 
 from ._prod_default import *

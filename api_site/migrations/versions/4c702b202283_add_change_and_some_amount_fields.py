24a2dafb4155a49228b7de58985610094fc1539c
yiwang <yiwang@lvye.com>
2015-11-17 16:24:15 +0800

    add change field to tx sn stack.

---
+++ api_site/migrations/versions/4c702b202283_add_change_and_some_amount_fields.py
@@ -0,0 +1,26 @@
+"""add change and some amount fields.
+
+Revision ID: 4c702b202283
+Revises: 4c8dbc7297f3
+Create Date: 2015-11-17 16:21:09.255994
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '4c702b202283'
+down_revision = '4c8dbc7297f3'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.add_column('payment_record', sa.Column('paid_amount', sa.Numeric(precision=12, scale=2), nullable=False, info={'after': 'amount'}))
+    op.add_column('payment_record', sa.Column('real_amount', sa.Numeric(precision=12, scale=2), nullable=False, info={'after': 'amount'}))
+    op.add_column('transaction_sn_stack', sa.Column('change', sa.Enum(u'EXPIRED', u'AMOUNT', u'INFO'), nullable=True, info={'after': 'state'}))
+
+
+def downgrade():
+    op.drop_column('transaction_sn_stack', 'change')
+    op.drop_column('payment_record', 'real_amount')
+    op.drop_column('payment_record', 'paid_amount')

f6a290b1fb213aa1868fccb1f4f9888641fad4a6
yiwang <yiwang@lvye.com>
2015-11-18 19:22:36 +0800

    use tx.super to get refund tx's payment tx.

--- api_site/src/api_x/zyt/biz/refund/__init__.py
+++ api_site/src/api_x/zyt/biz/refund/__init__.py
@@ -61,18 +61,20 @@ def handle_refund_notify(is_success, sn, vas_name, vas_sn, data):
     :return:
     """
     import time
-    for i in range(10):
-        # 这是因为_create_and_request_refund在一个事务中，notify到这儿的时候，事务没有结束，导致tx获取不到。
-        # 这里的解决办法就是重试
-        tx = get_tx_by_sn(sn)
-        if tx is None:
-            logger.warn('apply refund [{0}] is not finish, sleep then retry.')
-            time.sleep(0.5)
-            continue
-        refund_record = tx.record
-        break
+    tx = get_tx_by_sn(sn)
+    if tx is None:
+        for i in range(10):
+            # 这是因为_create_and_request_refund在一个事务中，notify到这儿的时候，事务没有结束，导致tx获取不到。
+            # 这里的解决办法就是重试
+            tx = get_tx_by_sn(sn)
+            if tx is None:
+                logger.warn('apply refund [{0}] is not finish, sleep then retry.')
+                time.sleep(0.5)
+                continue
+            break
+    refund_record = tx.record
 
-    payment_tx = get_payment_tx_by_sn(refund_record.payment_sn)
+    payment_tx = tx.super
     payment_record = payment_tx.record
 
     logger.info('refund notify: {0}, {1}, {2}, {3}, {4}'.format(is_success, sn, vas_name, vas_sn, data))

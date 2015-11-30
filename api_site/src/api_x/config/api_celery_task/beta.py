54d811783d41e0ca012d3be3b4086987703efd2e
yiwang <yiwang@lvye.com>
2015-11-30 16:45:23 +0800

    update celery config.

--- api_site/src/api_x/config/api_celery_task/beta.py
+++ api_site/src/api_x/config/api_celery_task/beta.py
@@ -8,7 +8,7 @@ CELERY_RESULT_BACKEND = 'amqp://lvye_pay:p@55word@127.0.0.1:5672/lvye_pay_beta'
 CELERYBEAT_SCHEDULE = {
     'test_job-E0.5d': {
         'task': 'api_x.task.tasks.test_job',
-        'schedule': timedelta(seconds=10),
+        'schedule': timedelta(seconds=60),
         'kwargs': {},
         'relative': True,
         'options': {'queue': 'celery_periodic'}

427cb5e5d8a34fcd7f60f02ed572bc6d39a5cd05
yiwang <yiwang@lvye.com>
2015-11-30 17:04:25 +0800

    update celery task beta config.

--- api_site/src/api_x/config/api_celery_task/beta.py
+++ api_site/src/api_x/config/api_celery_task/beta.py
@@ -5,6 +5,10 @@ BROKER_URL = 'amqp://lvye_pay:p@55word@127.0.0.1:5672/lvye_pay_beta'
 CELERY_RESULT_BACKEND = 'amqp://lvye_pay:p@55word@127.0.0.1:5672/lvye_pay_beta'
 
 
+CELERY_MAX_CACHED_RESULTS = 100
+CELERY_TASK_RESULT_EXPIRES = 600
+
+
 CELERYBEAT_SCHEDULE = {
     'test_job-E0.5d': {
         'task': 'api_x.task.tasks.test_job',

7ea39918b2cd2f2953b684d5055892f34a2200e2
yiwang <yiwang@lvye.com>
2016-01-24 14:51:27 +0800

    add expire_cheques script.

---
+++ api_site/scripts/expire_cheques.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+env=${1:-dev}
+name=${2:-api_venv}
+
+PROJ_ROOT=$(dirname $(dirname $0))
+VENV_DIR=${PROJ_ROOT}/${name}
+
+source ${PROJ_ROOT}/scripts/common.sh
+# activate the virtualenv
+source ${VENV_DIR}/bin/activate
+
+${PROJ_ROOT}/src/manager.py -e ${env} expire_cheques
